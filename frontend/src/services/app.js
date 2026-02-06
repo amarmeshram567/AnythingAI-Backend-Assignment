b0cfbc4d6b2e164443296d3db62f006bec7b9193
amarmeshram567 <meshramamar567@gmail.com>
2026-02-06 22:47:21 +0530

    url added

--- frontend/src/services/app.js
+++ frontend/src/services/app.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000',
+    baseURL: import.meta.env.VITE_BACKEND_URL || 'https://anythingai-backend-assignment-1.onrender.com',
     headers: {
         "Content-Type": "application/json"
     },
